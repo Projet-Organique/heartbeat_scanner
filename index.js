bfb08c38563bcfb375468e9ab0bc0cb7f8cd020e
John Barry <john.olivierb@gmail.com>
2021-11-16 19:47:58 -0500

    Setup timer and basic bpm waiting logic

--- index.js
+++ index.js
@@ -1,73 +1,72 @@
-require('dotenv').config()
-const {
-  createBluetooth
-} = require('./src')
+require("dotenv").config();
+const { createBluetooth } = require("./src");
 var { Timer } = require("easytimer.js");
 var timerInstance = new Timer();
-const {
-  POLAR_MAC_ADRESSE
-} = process.env
+const { POLAR_MAC_ADRESSE } = process.env;
 
+let _HEARTRATE = null;
+let _USERBPM = 0;
 
-function errror(message) {
-  this.message = message;
-}
 async function connectDevice() {
 
-  const { bluetooth, destroy } = createBluetooth()
-  const adapter = await bluetooth.defaultAdapter()
+  const { bluetooth, destroy } = createBluetooth();
+  const adapter = await bluetooth.defaultAdapter();
 
-  if (!await adapter.isDiscovering())
-    await adapter.startDiscovery()
-  console.log('Discovering device...');
+  if (!(await adapter.isDiscovering())) 
+    await adapter.startDiscovery();
+  console.log("Discovering device...");
 
-  const device = await adapter.waitDevice("A0:9E:1A:9F:0E:B4")
-  console.log('got device', await device.getAddress(), await device.getName())
-  await device.connect()
-  console.log('Connected!')
+  const device = await adapter.waitDevice("A0:9E:1A:9F:0E:B4");
+  console.log("got device", await device.getAddress(), await device.getName());
+  await device.connect();
+  console.log("Connected!");
 
-  const gattServer = await device.gatt()
-  var services = await gattServer.services();
+  const gattServer = await device.gatt();
+  //var services = await gattServer.services();
 
-  console.log(services)
-
-  const service = await gattServer.getPrimaryService("0000180d-0000-1000-8000-00805f9b34fb")
+  const service = await gattServer.getPrimaryService(
+    "0000180d-0000-1000-8000-00805f9b34fb"
+  );
   //const char_list = await service.characteristics();
   //console.log(char_list);
-  const heartrate = await service.getCharacteristic("00002a37-0000-1000-8000-00805f9b34fb")
+  const heartrate = await service.getCharacteristic(
+    "00002a37-0000-1000-8000-00805f9b34fb"
+  );
+
+  _HEARTRATE = heartrate;
+
   await heartrate.startNotifications();
-  console.log("Ready to scan...");
 
-  getBeatPerMinute(device);
+  getBpm().then(console.log).catch(console.error);
+
+  //await heartrate.stopNotifications();
+  //return BPM
 }
 
-async function getBeatPerMinute(device) {
-  // Before getting the BPM we need to get a user await getUser();
 
-  timerInstance.start({ countdown: true, startValues: { seconds: 3 } });
-  timerInstance.addEventListener('secondsUpdated', function (e) {
-    console.log(timerInstance.getTimeValues().toString());
+
+async function getBpm() {
+  return new Promise((resolve, reject) => {
+
+    timerInstance.addEventListener("secondsUpdated", function (e) {
+      console.log(timerInstance.getTimeValues().toString());
+    });
+
+    timerInstance.addEventListener("targetAchieved", async function (e) {
+      resolve(_USERBPM);
+    });
+
+    _HEARTRATE.on("valuechanged", (buffer) => {
+      let json = JSON.stringify(buffer);
+      var bpm = Math.max.apply(null, JSON.parse(json).data);
+      _USERBPM = bpm;
+      if (bpm != 0) {
+        timerInstance.start({ countdown: true, startValues: { seconds: 3 } });
+      } else {
+        console.log("BPM is " + bpm + ", waiting value before timer...");
+      }
+    });
   });
-  timerInstance.addEventListener('targetAchieved', function (e) {
-    console.log("Done scanning")
-  });
-
-  //------- part where we get the GATT data.
-  // When value successfull we wait for a stable value between 40 and 120
-  // then we start the timer for at least 5sec
-  // and we return the value BPM
-
-  heartrate.on('valuechanged', buffer => {
-    let json = JSON.stringify(buffer);
-    console.log(JSON.parse(json).data);
-    return JSON.parse(json).data
-    //done()
-  })
-
-  //await characteristic2.stopNotifications()
-  // destroy()
-
 }
 
-//main().then(console.log).catch(console.log);
-connectDevice().then(console.log).catch(console.error)
+connectDevice().then(console.log).catch(console.error);
