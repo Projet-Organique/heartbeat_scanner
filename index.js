828082c6355a5875e37819cc7d86bd215f9baa01
John <john.olivierb@gmail.com>
2021-11-18 21:50:32 -0500

    Added a checkup for program looping

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ var { Timer } = require("easytimer.js");
 var timerInstance = new Timer();
 const { POLAR_MAC_ADRESSE } = process.env;
 
+let _USERBPM;
 let _HEARTRATE = null;
-let _USERBPM = 0;
+let BPMNOTZERO = false;
 
 async function connectDevice() {
-
+ 
+  //console.log('process.env.USER ', process.env.TT );
+  //process.env.TT = true;
+  console.log('process.env.TT', process.env.TT);
   const { bluetooth, destroy } = createBluetooth();
   const adapter = await bluetooth.defaultAdapter();
 
@@ -22,13 +26,23 @@ async function connectDevice() {
   console.log("Connected!");
 
   const gattServer = await device.gatt();
-  //var services = await gattServer.services();
+ // console.log('gattServer', gattServer._services);
+  var services = await gattServer.services();
+
+
+//  const service1 = await gattServer.getPrimaryService('0000180d-0000-1000-8000-00805f9b34fb')
+// const characteristic1 = await service1.getCharacteristic('00002a37-0000-1000-8000-00805f9b34fb')
+// await characteristic1.writeValue(Buffer.from("Hello world"))
+// const buffer = await characteristic1.readValue()
+// console.log(buffer)
+
+  console.log('services', services.toString());
 
   const service = await gattServer.getPrimaryService(
     "0000180d-0000-1000-8000-00805f9b34fb"
   );
-  //const char_list = await service.characteristics();
-  //console.log(char_list);
+
+
   const heartrate = await service.getCharacteristic(
     "00002a37-0000-1000-8000-00805f9b34fb"
   );
@@ -36,18 +50,39 @@ async function connectDevice() {
   _HEARTRATE = heartrate;
 
   await heartrate.startNotifications();
-
-  getBpm().then(console.log).catch(console.error);
-
+  console.log('LOADING...');
+  //setTimeout(async () => {
+    let checkBPM = await checkBpm();
+    console.log('READY')
+    if(checkBPM){
+      _USERBPM = await getBpm();
+      console.log('bpm is:', _USERBPM);
+      process.exit(1);
+    }
+  //}, 15000);
+    //process.exit(1);
   //await heartrate.stopNotifications();
   //return BPM
 }
 
+async function checkBpm(){
+  return new Promise((resolve, reject) => {
+    _HEARTRATE.on("valuechanged", (buffer) => {
+      let json = JSON.stringify(buffer);
+      let bpm = Math.max.apply(null, JSON.parse(json).data);
+      console.log('bpm', bpm);
+      //console.log('bpm', bpm);
+      if(bpm == 0){
+        resolve(true)
+      }
+    })
 
+  })
+}
 
 async function getBpm() {
   return new Promise((resolve, reject) => {
-
+    let _USERBPM
     timerInstance.addEventListener("secondsUpdated", function (e) {
       console.log(timerInstance.getTimeValues().toString());
     });
@@ -58,13 +93,11 @@ async function getBpm() {
 
     _HEARTRATE.on("valuechanged", (buffer) => {
       let json = JSON.stringify(buffer);
-      var bpm = Math.max.apply(null, JSON.parse(json).data);
-      _USERBPM = bpm;
-      if (bpm != 0) {
-        timerInstance.start({ countdown: true, startValues: { seconds: 3 } });
-      } else {
-        console.log("BPM is " + bpm + ", waiting value before timer...");
-      }
+      let bpm = Math.max.apply(null, JSON.parse(json).data);
+      if(bpm != 0){
+        _USERBPM = bpm;
+        timerInstance.start({ countdown: true, startValues: { seconds: 10 } });
+      } 
     });
   });
 }
