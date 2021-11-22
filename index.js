15214bcea7bd9f3767777c4cc415a6ee717f03d3
John <john.olivierb@gmail.com>
2021-11-22 11:36:40 -0500

    Builded pipeline for scan and put

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ const { createBluetooth } = require("./src");
 var { Timer } = require("easytimer.js");
 var timerInstance = new Timer();
 const axios = require('axios');
-const { POLAR_MAC_ADRESSE, RANDOMUSER_ENDPOINT } = process.env;
+const { POLAR_MAC_ADRESSE, RANDOMUSER_ENDPOINT, UPDATEUSER_ENDPOINT } = process.env;
 
 let _USERBPM;
 let _USER;
 let _HEARTRATE = null;
 let BPMNOTZERO = false;
 
+
+
 async function connectDevice() {
-  _USER = await axios.get(RANDOMUSER_ENDPOINT)
-  console.log('_USER', _USER.data);
-  console.log('RANDOMUSER_ENDPOINT', RANDOMUSER_ENDPOINT);
-  //console.log('_USER', _USER);
+  console.clear();
   const { bluetooth, destroy } = createBluetooth();
   const adapter = await bluetooth.defaultAdapter();
 
@@ -28,17 +27,7 @@ async function connectDevice() {
   console.log("Connected!");
 
   const gattServer = await device.gatt();
-
- // console.log('gattServer', gattServer._services);
   var services = await gattServer.services();
-  console.log('services', services);
-
-//  const service1 = await gattServer.getPrimaryService('0000180d-0000-1000-8000-00805f9b34fb')
-// const characteristic1 = await service1.getCharacteristic('00002a37-0000-1000-8000-00805f9b34fb')
-// await characteristic1.writeValue(Buffer.from("Hello world"))
-// const buffer = await characteristic1.readValue()
-// console.log(buffer)
-
 
  const service = await gattServer.getPrimaryService(
     "0000180d-0000-1000-8000-00805f9b34fb"
@@ -47,25 +36,40 @@ async function connectDevice() {
     "00002a37-0000-1000-8000-00805f9b34fb"
   );
 
- //const buffer = await heartrate.readValue()
- //console.log(buffer)
   _HEARTRATE = heartrate;
 
-  await heartrate.startNotifications();
-  console.log('LOADING...');
+  var twirlTimer = (function() {
+    var P = ["\\", "|", "/", "-"];
+    var x = 0;
+    return setInterval(function() {
+      process.stdout.write("\r" + P[x++] + ' LOADING...');
+      x &= 3;
+    }, 250);
+  })();
   //setTimeout(async () => {
     /*console.log('Current BPM is: ' + await getCurrentBpm());
     if(await getCurrentBpm() != 0){
       console.log('Waiting...')
     }*/
+    _USER = await axios.get(RANDOMUSER_ENDPOINT).catch(function (error) {
+    if (error.response) {
+      console.log(error.response.data.message);
+      process.exit(1);
+    }
+    });
+    console.log('_USER', _USER.data._id );
+    //console.log('_USER', _USER);
+    await heartrate.startNotifications();
     const checkBPM = await checkBpm();
-   
+    _HEARTRATE.stopNotifications();
     if(checkBPM){
-      console.log('checkBPM', checkBPM);
-      console.log('Ready...!')
+      clearInterval(twirlTimer)
+      process.stdout.write("\r\x1b[K")
+      process.stdout.write('Ready!')
+      //set a presence detection to start notification
       _USERBPM = await getBpm();
-      await axios.POST(UPDATEUSER_ENDPOINT + _USER.ID, {'pulse': _USERBPM})
-
+      console.log('_USERBPM', _USERBPM);
+      await axios.put(UPDATEUSER_ENDPOINT + _USER.data._id, {'pulse': _USERBPM})
       process.exit(1);
     }
   //}, 15000);
@@ -74,34 +78,21 @@ async function connectDevice() {
   //return BPM
 }
 
-async function polarEventListener(){
-  let currentBpm;
-  return new Promise(async (resolve, reject) => {
-    _HEARTRATE.on("valuechanged", async (buffer) => {
-      console.log('buffer', buffer);
-      let json = JSON.stringify(buffer);
-      let bpm = Math.max.apply(null, JSON.parse(json).data);
-      resolve(bpm);
-    })
-  })
-
-}
-
-async function getCurrentBpm(){
-  return new Promise(async (resolve, reject) => {
-      let bpm = await polarEventListener();
-      resolve(bpm)
-  })
-}
 
 
 async function checkBpm(){
   return new Promise(async (resolve, reject) => {
-      let bpm = await polarEventListener();
+    _HEARTRATE.on("valuechanged", async (buffer) => {
+      let json = JSON.stringify(buffer);
+      let bpm = Math.max.apply(null, JSON.parse(json).data);
+
+   
+      
 
       if(bpm == 0){
         resolve(true)
       }
+    })
   })
 }
 
@@ -109,18 +100,34 @@ async function getBpm() {
   return new Promise(async (resolve, reject) => {
     let _USERBPM
     timerInstance.addEventListener("secondsUpdated", function (e) {
-      console.log(timerInstance.getTimeValues().toString());
+      //console.log(timerInstance.getTimeValues().toString());
+
+        process.stdout.write(timerInstance.getTimeValues().toString() + '\n');
+      //process.stdout.write("\r\x1b[K")
     });
 
     timerInstance.addEventListener("targetAchieved", async function (e) {
       resolve(_USERBPM);
     });
 
-    let bpm = await polarEventListener();
-      if(bpm != 0){
-        _USERBPM = bpm;
-        timerInstance.start({ countdown: true, startValues: { seconds: 10 } });
-      } 
+    //let bpm = await polarEventListener();
+    _HEARTRATE.startNotifications();
+      _HEARTRATE.on("valuechanged", async (buffer) => {
+        let json = JSON.stringify(buffer);
+        let bpm = Math.max.apply(null, JSON.parse(json).data);
+      
+   
+  process.stdout.write('BPM: ' + bpm + '\n');
+          //process.stdout.write("\r\x1b[K")
+  
+     
+        if(bpm != 0){
+          _USERBPM = bpm;
+          timerInstance.start({ countdown: true, startValues: { seconds: 10 } });
+        } 
+      })
+
+     
 
   });
 }
